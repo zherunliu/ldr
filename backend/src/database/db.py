8f0a47e9d5271941f80d366ef258290be8742a8b
zherunliu <1437257281@qq.com>
2025-10-26 21:35:53 +0800

    format

--- backend/src/database/db.py
+++ backend/src/database/db.py
@@ -22,9 +22,9 @@ def create_challenge_quota(db: Session, user_id: str):
 
 def reset_quota(db: Session, quota: models.ChallengeQuota):
     now = datetime.now()
-    if now - quota.last_reset_date > timedelta(hours=24): # type: ignore
-        quota.quota_remaining = 10 # type: ignore
-        quota.last_reset_date = now # type: ignore
+    if now - quota.last_reset_date > timedelta(hours=24):  # type: ignore
+        quota.quota_remaining = 10  # type: ignore
+        quota.last_reset_date = now  # type: ignore
         db.commit()
         db.refresh(quota)
     return quota
